2f5a7f844516b09be2b8f0cf2e15d9bfdcc89cb7
Tarek592 <muhammadtarek5920@gmail.com>
2025-03-01 13:20:14 +0600

    increment and decrement number complete

--- script.js
+++ script.js
@@ -1,2 +1,28 @@
-let tarek=document.getElementById("tarek").innerText=50
-console.log(tarek)
+// color btn start
+
+let color=document.getElementById("colorBtn").addEventListener("click", function(){
+    getColor()
+})
+
+function getColor(){
+    let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = randomColor;
+}
+
+// color btn close
+
+ 
+ document.getElementById("cardBtn-1").addEventListener('click',function(){
+    let taskNumber=parseInt(document.getElementById("taskNumber").innerText);
+    let task=taskNumber-1;
+    document.getElementById("taskNumber").innerText=task;
+    let updateNumber=parseInt(document.getElementById("updateNumber").innerText);
+    let update=updateNumber+1
+    document.getElementById("updateNumber").innerText=update;
+    alert("board updated successfully")
+   
+
+ })
+ function disableButton(button) {
+    button.disabled = true;
+}
